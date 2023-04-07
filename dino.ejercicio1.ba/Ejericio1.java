7211d7a1128b8b220d62cac7eb2a86dc3fe2ab50
dinobossytnew <58078116+dinobossytnew@users.noreply.github.com>
2023-04-07 18:21:21 -0300

    Create Ejericio1.java
    Manda la lista de departamentos de uruguay.

---
+++ dino.ejercicio1.ba/Ejericio1.java
@@ -0,0 +1,30 @@
+package dino.ejercicio1.ba;
+
+public class Ejericio1 {
+
+	public static void main(String[] args) {
+		// objetivo
+		// 1. Muestre en la pantalla los nombres de los 19 departamentos del Uruguay.
+		System.out.println("Artigas");
+		System.out.println("Salto");
+		System.out.println("Paysadu");
+		System.out.println("Rio Negro");
+		System.out.println("Colonia");
+		System.out.println("Soriano");
+		System.out.println("Rivera");
+		System.out.println("Tacuarembo");
+		System.out.println("Durazo");
+		System.out.println("Flores");
+		System.out.println("Florida");
+		System.out.println("San Jose");
+		System.out.println("Canelones");
+		System.out.println("Montevideo");
+		System.out.println("Cerro Largo");
+		System.out.println("Treinta Y Tres");
+		System.out.println("LaValleja");
+		System.out.println("Maldonado");
+		System.out.println("Rocha");
+
+	}
+
+}
