9aa4c6319e7ca72050e40c6df07f7ce0c6a3f130
dinobossytnew <58078116+dinobossytnew@users.noreply.github.com>
2023-04-07 18:24:01 -0300

    Create Ejercico2.java
    Comprobamos un ratito? codigo busca diferencia entre numberos pero antes hace suma , mutiplica y divide

---
+++ dino.ejericico.ba3/Ejercico2.java
@@ -0,0 +1,22 @@
+package dino.ejericico.ba3;
+
+public class Ejercico2 {
+
+	public static void main(String[] args) {
+		int num1 = 10, num2 = 10;
+    
+		//Debug
+		System.out.println(num1+num2);
+		System.out.println(num1*num2);
+		System.out.println(num1/num2);
+		
+		if(num1!=num2) {
+			System.out.println("Son diferente "+ num1 +" Es mayor que "+ num2);
+		}else {
+			System.out.println(num1+ " y "+ num1 +" Son iguales GG ");
+		}
+		
+		
+	}
+
+}
